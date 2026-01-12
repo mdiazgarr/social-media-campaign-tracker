50196bd006cefc3c492528541ba7ce0c36723f2a
mdiazgarr <mariadiazgarr@gmail.com>
2026-01-12 09:27:24 -0500

    Deleted unnecessary files

--- voter_analytics/urls.py
+++
@@ -1,9 +0,0 @@
-# voter_analytics/urls.py
-from django.urls import path
-from .views import * 
-
-urlpatterns = [
-    path('', VoterListView.as_view(), name="voters"),
-    path('voter/<int:pk>/', VoterDetailView.as_view(), name="voter"),
-    path("graphs/", VoterGraphsView.as_view(), name="graphs"),
-]
